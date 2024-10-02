01f728a0e475fe20bf2c1a4c9c198bcb13b831a0
Hillary Malespin Ulloa <80055697+HillaryMalespin@users.noreply.github.com>
2024-10-01 21:52:41 -0600

    Se implementó consultar reserva

--- Programa/opcionesGenerales.hs
+++ Programa/opcionesGenerales.hs
@@ -3,10 +3,9 @@
 
 module OpcionesGenerales where
 
-import Control.Concurrent (threadDelay)
 import Data.Aeson (decode, encode, ToJSON, FromJSON)
 import GHC.Generics (Generic)
-import qualified Data.ByteString.Lazy as B
+import qualified Data.ByteString.Lazy as BL  -- Importa correctamente BL
 import Data.List (find)
 import Data.Maybe (isNothing)
 import Control.Monad (when)
@@ -14,7 +13,7 @@ import Control.Monad (when)
 -- Definir las estructuras de datos para Reserva y Sala
 data Reserva = Reserva 
     { codigoReserva :: String
-    , codigoSalaReserva :: String  -- Cambiado el nombre del campo
+    , codigoSala :: String  -- Este es el campo correcto para obtener el código de la sala en Reserva
     , fecha :: String
     , cantidadPersonas :: Int
     } deriving (Show, Eq, Generic)
@@ -23,7 +22,7 @@ instance ToJSON Reserva
 instance FromJSON Reserva
 
 data Sala = Sala 
-    { codigoSala :: String
+    { codigoSalaLocal :: String
     , capacidad :: Int
     , edificio :: String
     , mobiliarioSeleccionado :: [String]
@@ -38,23 +37,20 @@ instance FromJSON Sala
 -- Función para cargar reservas desde un archivo JSON
 cargarReservas :: IO (Maybe [Reserva])
 cargarReservas = do
-    contenido <- B.readFile "reservas.json"
+    contenido <- BL.readFile "reservas.json"  -- Usar BL.readFile
     let reservas = decode contenido :: Maybe [Reserva]
     return reservas
 
 -- Función para cargar salas desde un archivo JSON
 cargarSalas :: IO (Maybe [Sala])
 cargarSalas = do
-    contenido <- B.readFile "salas.json"
+    contenido <- BL.readFile "salas.json"  -- Usar BL.readFile
     let salas = decode contenido :: Maybe [Sala]
     return salas
 
--- guardar reservas
+-- Función para guardar reservas en un archivo JSON
 guardarReservas :: [Reserva] -> IO ()
-guardarReservas reservas = do
-    threadDelay 1000000  -- Pausa de 1 segundo antes de intentar escribir
-    B.writeFile "reservas.json" (encode reservas)
-
+guardarReservas reservas = BL.writeFile "reservas.json" (encode reservas)
 
 -- Crear una nueva reserva después de verificar disponibilidad
 crearReserva :: String -> String -> String -> Int -> IO ()
@@ -64,7 +60,7 @@ crearReserva idUsuario idSala fechaSolicitada cantidad = do
     let reservas = maybe [] id maybeReservas
     
     -- Verificar si la sala ya tiene una reserva en la fecha solicitada
-    let reservaExistente = find (\r -> codigoSalaReserva r == idSala && fecha r == fechaSolicitada) reservas
+    let reservaExistente = find (\r -> codigoSala r == idSala && fecha r == fechaSolicitada) reservas
     
     -- Cargar las salas desde el archivo
     maybeSalas <- cargarSalas
@@ -73,15 +69,15 @@ crearReserva idUsuario idSala fechaSolicitada cantidad = do
     if isNothing reservaExistente
         then do
             -- Verificar si la sala existe y tiene capacidad suficiente
-            let sala = find (\s -> codigoSala s == idSala) salas
+            let sala = find (\s -> codigoSalaLocal s == idSala) salas
             case sala of
                 Just s -> do
-                    putStrLn $ "Sala encontrada: " ++ codigoSala s ++ " con capacidad: " ++ show (capacidad s)
+                    putStrLn $ "Sala encontrada: " ++ codigoSalaLocal s ++ " con capacidad: " ++ show (capacidad s)
                     if capacidad s >= cantidad
                         then do
                             -- Crear un nuevo código de reserva
                             let nuevoCodigoReserva = "R" ++ show (length reservas + 1)
-                            let nuevaReserva = Reserva { codigoReserva = nuevoCodigoReserva, codigoSalaReserva = idSala, fecha = fechaSolicitada, cantidadPersonas = cantidad }
+                            let nuevaReserva = Reserva { codigoReserva = nuevoCodigoReserva, codigoSala = idSala, fecha = fechaSolicitada, cantidadPersonas = cantidad }
                             -- Agregar la nueva reserva y guardar en el archivo
                             let reservasActualizadas = nuevaReserva : reservas
                             guardarReservas reservasActualizadas
@@ -93,11 +89,19 @@ crearReserva idUsuario idSala fechaSolicitada cantidad = do
 -- Consultar una reserva por código
 consultarReserva :: String -> IO ()
 consultarReserva codigo = do
+    -- Cargar reservas desde el archivo JSON
     maybeReservas <- cargarReservas
     let reservas = maybe [] id maybeReservas
+    -- Buscar la reserva por el código
     let reserva = find (\r -> codigoReserva r == codigo) reservas
+    -- Mostrar los detalles de la reserva o mensaje si no existe
     case reserva of
-        Just r -> putStrLn $ "Detalles de la reserva: " ++ show r
+        Just r -> do
+            putStrLn "Reserva encontrada:"
+            putStrLn $ "Código de reserva: " ++ codigoReserva r
+            putStrLn $ "Código de sala: " ++ codigoSala r  -- Usar correctamente el campo codigoSala en Reserva
+            putStrLn $ "Fecha de reserva: " ++ fecha r
+            putStrLn $ "Cantidad de personas: " ++ show (cantidadPersonas r)
         Nothing -> putStrLn "Reserva no encontrada."
 
 -- Cancelar una reserva por código
@@ -115,7 +119,7 @@ modificarReserva codigo idSala nuevaFecha nuevaCantidad = do
     maybeReservas <- cargarReservas
     let reservas = maybe [] id maybeReservas
     let reservasModificadas = map (\r -> if codigoReserva r == codigo 
-                                          then r { codigoSalaReserva = idSala, fecha = nuevaFecha, cantidadPersonas = nuevaCantidad } 
+                                          then r { codigoSala = idSala, fecha = nuevaFecha, cantidadPersonas = nuevaCantidad } 
                                           else r) reservas
     guardarReservas reservasModificadas
     putStrLn $ "Reserva modificada a: Sala: " ++ idSala ++ ", Fecha: " ++ nuevaFecha ++ ", Cantidad: " ++ show nuevaCantidad
